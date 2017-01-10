cea83a94bf334bbf34fd2e31fbcb8cfb77053296
周洪昌 <zhouhongchang@n2n2n.com>
2017-01-10 16:13:46 +0800

    add depends

--- Application/Api/Controller/GoodsController.class.php
+++ Application/Api/Controller/GoodsController.class.php
@@ -3,19 +3,6 @@ namespace Api\Controller;
 use Think\Controller\RestController;
 
 class GoodsController extends RestController {
-    Public function rest() {
-        switch ($this->_method){
-        case 'get': // get请求处理代码
-            break;
-        case 'put': // put请求处理代码
-            break;
-        case 'delete':
-            break;
-        case 'post': // post请求处理代码
-            break;
-        }
-    }
-    
     Public function items()
     {
         $goods = D('Common/Goods')->limit(10)->field('id, goods_name, add_time')->select();
--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,7 +3,5 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-        $good = D('Common/Goods')->find(1);
-        var_dump($good);
     }
 }
